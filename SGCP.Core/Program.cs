8aac40368099c2e8298d0db7d6c5e3864cebdd9b
MoisesBrito31 <moisesbritofuentes@gmail.com>
2020-01-24 20:33:08 -0300

    Create Program.cs

---
+++ SGCP.Core/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SGCP.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+           .UseUrls("https://*:5000;https://localhost:5000")           
+           .UseSockets()
+               .UseStartup<Startup>();
+    }
+}
